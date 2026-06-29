9d7227be83d1feb25c05ec78940aed17d8053715
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-06-29 21:39:52 +0600

    remove data

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default async function RootLayout({ children }) {
     <html
       lang="en"
       suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased `}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground ">
         <Providers>
